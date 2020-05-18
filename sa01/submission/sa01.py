9f5d41d6c3f75b5903475d15416bc5f477c47dea
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2020-05-18 09:56:08 -0300

    sa01 successfully done

--- sa01/submission/sa01.py
+++ sa01/submission/sa01.py
@@ -9,18 +9,22 @@ import imageio
 # import matplotlib.pyplot as plt
 
 
+r = imageio.imread(str(input()).rstrip()).astype(np.uint8)
+tin = float(input())
+
+
 # definition of 2D Fourier Transformation, as taught on lectures
 def DFT2D(f, inverse=False):
     F = np.zeros(f.shape, dtype=np.complex64)
     n, m = f.shape[0:2]
     sign = (-1, 1)[inverse]
 
+    x = np.arange(n).reshape(n, 1)
+    y = np.arange(m).reshape(1, m)
     for u in np.arange(n):
         for v in np.arange(m):
-            for x in np.arange(n):
-                for y in np.arange(m):
-                    F[u, v] += f[x, y] * \
-                        np.exp((sign*1j*2*np.pi)*(((u*x)/n)+((v*y)/m)))
+            F[u, v] += np.sum(f[x, y] *
+                              np.exp((sign*1j*2*np.pi)*(((u*x)/n)+((v*y)/m))))
 
     return F/np.sqrt(n*m)
 
@@ -42,18 +46,21 @@ def filter(f, threshold):
 
 def imgMean(f):
     n, m = f.shape[0:2]
-    mean = 0j
-    for x in np.arange(n):
-        for y in np.arange(m):
-            mean += f[x, y]
-    return np.abs(mean/(n*m))
-
-
-r = imageio.imread(str(input()).rstrip()).astype(np.uint8)
-tin = float(input())
+    x = np.arange(n).reshape(n, 1)
+    y = np.arange(m).reshape(1, m)
+    mean = np.sum(f[x, y])
+    return np.abs(np.real(mean)/(n*m))
 
 
 R = DFT2D(np.asarray(r))
+
+
+# plt.subplot(121)
+# plt.imshow(r, cmap="gray")
+# plt.axis('off')
+# plt.subplot(122)
+# plt.imshow(np.log(1 + np.abs(R)), cmap="gray")
+# plt.axis('off')
 
 
 sndMax = np.sort(np.abs(R.flatten()))[-2]
@@ -64,26 +71,26 @@ countFiltered, Rfiltered = filter(R, threshold)
 
 
 # plt.subplot(121)
-# plt.imshow(r, cmap="gray")
+# plt.imshow(np.log(1 + np.abs(R)), cmap="gray")
 # plt.axis('off')
 # plt.subplot(122)
 # plt.imshow(np.log(1 + np.abs(Rfiltered)), cmap="gray")
 # plt.axis('off')
 
 
-invRfiltered = DFT2D(np.asarray(Rfiltered), True)
+rfiltered = DFT2D(np.asarray(Rfiltered), True)
 
 
 # plt.subplot(121)
 # plt.imshow(r, cmap="gray")
 # plt.axis('off')
 # plt.subplot(122)
-# plt.imshow(np.log(1 + np.abs(invRfiltered)), cmap="gray")
+# plt.imshow(np.log(1 + np.abs(rfiltered)), cmap="gray")
 # plt.axis('off')
 
 
 print("Threshold={:.4f}".format(threshold))
 print("Filtered Coefficients=", countFiltered, sep='')
-print("Original Mean={:.2f}".format(
-    np.abs(R[0, 0]/np.sqrt(r.shape[0]*r.shape[1]))))
-print("New Mean={:.2f}".format(imgMean(invRfiltered)))
+print("Original Mean={:.2f}".format(imgMean(r))) # I know it's the same as R[0,0]
+print("New Mean={:.2f}".format(imgMean(np.abs(rfiltered))))
+
